a036b3c5a09ef2038d367d289c88ec54b56f0e21
Chiamaka Ikeanyi <chiamakaikeanyi@gmail.com>
2019-04-04 18:48:40 +0100

    Add sieve of eratosthenes

---
+++ sieveOfEratosthenes.js
@@ -0,0 +1,23 @@
+const sieveOfEratosthenes = (n) => {
+  var primes = new Array(n).fill(true);
+
+  primes[0] = false;
+  primes[1] = false;
+
+  for (let i = 2; i < Math.sqrt(n); i++) {
+    for (let j = 2; j * i <= n; j++) {
+      primes[i * j] = false;
+    }
+  }
+
+  let result = [];
+  for (let i = 0; i < primes.length; i++) {
+    if (primes[i]) {
+      result.push(i)
+    }
+  }
+
+  return result;
+}
+
+sieveOfEratosthenes(10)
