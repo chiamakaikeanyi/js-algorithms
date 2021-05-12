64eec1f60cc03487ac257acfd26eab5830753fb9
Chiamaka Ikeanyi <chiamakaikeanyi@gmail.com>
2021-05-12 04:38:34 +0100

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -15,3 +15,23 @@ const twoSum = (numArray, sum) => {
 }
 
 twoSum([1, 2, 3, 4, 5, 6, 7], 7);
+
+
+
+function findPairsThatAddUpToSum(array, sum) {
+  const result = [];
+  const map = {};
+  
+  for(const value of array) {
+    if(value in map){
+      result.push([value, map[value]]);
+    } else {
+      const counterpart = sum - value;
+      map[counterpart] = value;
+    }
+  }
+  
+  return result;
+}
+
+findPairsThatAddUpToSum([1, 2, 3, 4, 5, 6, 7], 7);
