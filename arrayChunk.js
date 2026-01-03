71973adfffe351165661a11b9c7dd4cad14e2dc7
Chiamaka Ikeanyi <chiamakaikeanyi@gmail.com>
2026-01-03 14:52:48 +0100

    Add more DSA

---
+++ arrayChunk.js
@@ -0,0 +1,96 @@
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+// Soln 1
+function chunk(array, size) {
+  const chunks = [];
+  const clonedArray = [...array];
+
+  while (clonedArray.length > 0) {
+    chunks.push(clonedArray.splice(0, size));
+  }
+
+  return chunks;
+}
+
+// Soln 2
+function chunk(array, size) {
+  const chunked = [];
+
+  for (let element of array) {
+    const last = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+
+  return chunked;
+}
+
+// Soln 3
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size)); // e.g. [2,4,6].slice(0,2) => slice start to end (end not included) = [2,4];
+    index += size;
+  }
+
+  return chunked;
+}
+
+// Test Cases
+test("function chunk exists", () => {
+  expect(typeof chunk).toEqual("function");
+});
+
+test("chunk divides an array of 10 elements with chunk size 2", () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const chunked = chunk(arr, 2);
+
+  expect(chunked).toEqual([
+    [1, 2],
+    [3, 4],
+    [5, 6],
+    [7, 8],
+    [9, 10],
+  ]);
+});
+
+test("chunk divides an array of 3 elements with chunk size 1", () => {
+  const arr = [1, 2, 3];
+  const chunked = chunk(arr, 1);
+
+  expect(chunked).toEqual([[1], [2], [3]]);
+});
+
+test("chunk divides an array of 5 elements with chunk size 3", () => {
+  const arr = [1, 2, 3, 4, 5];
+  const chunked = chunk(arr, 3);
+
+  expect(chunked).toEqual([
+    [1, 2, 3],
+    [4, 5],
+  ]);
+});
+
+test("chunk divides an array of 13 elements with chunk size 5", () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  const chunked = chunk(arr, 5);
+
+  expect(chunked).toEqual([
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13],
+  ]);
+});
